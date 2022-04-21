808b0fff500197c728f7f9c7ba55b5ed78e9068e
BILIBILI\wangyudi <wangyudi@bilibili.com>
2022-04-21 15:42:11 +0800

    完成矩阵运算顶点的位移、旋转、缩放

--- Beginner.cpp
+++ Beginner.cpp
@@ -1,5 +1,4 @@
-﻿
-#include <glad/glad.h>
+﻿#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
@@ -15,7 +14,7 @@ using namespace std;
 
 GLFWwindow* window;
 
-void loadTextureFromResource(const string & pic_name, const GLint internal_format, const GLenum format)
+void loadTextureFromResource(const string& pic_name, const GLint internal_format, const GLenum format)
 {
 	string path = "Resource/" + pic_name;
 
@@ -48,31 +47,33 @@ int main()
 
 	window = glfwCreateWindow(640, 640, "Hello World", nullptr, nullptr);
 
-	if (!window) {
+	if (!window)
+	{
 		glfwTerminate();
 		return -1;
 	}
 
 	glfwMakeContextCurrent(window);
 
-	if (!gladLoadGL()) {
+	if (!gladLoadGL())
+	{
 		cout << "GLAD加载函数失败" << endl;
 	}
 
 	// 定义要绘制的顶点信息
-	constexpr float verticals[] = 
+	constexpr float verticals[] =
 	{
 		//     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
-			 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
-			 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
-			-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
-			-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f,    // 左上
+		0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // 右上
+		0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // 右下
+		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // 左下
+		-0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, // 左上
 	};
 
 	const unsigned int indices[] =
 	{
-		0,1,2,
-		0,2,3,
+		0, 1, 2,
+		0, 2, 3,
 	};
 
 	// 绘制时需要的操作对象
@@ -100,7 +101,7 @@ int main()
 	glEnableVertexAttribArray(2);
 
 	// 准备ShaderProgram
-	Shader shader("Shader/Texture.vs","Shader/Texture.fs");
+	Shader shader("Shader/Texture.vs", "Shader/Texture.fs");
 	if (shader.ID == 0)
 	{
 		cout << "there is no shader program" << endl;
@@ -109,33 +110,35 @@ int main()
 
 	// 生成纹理
 	unsigned int texture, texture2;
-	glGenTextures(1, &texture);
-	glGenTextures(1, &texture2);
+	{
+		glGenTextures(1, &texture);
+		glGenTextures(1, &texture2);
 
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, texture);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture);
 
-	// 设置纹理边缘的重复规则
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
-	// 设置纹理过滤
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		// 设置纹理边缘的重复规则
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+		// 设置纹理过滤
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	// 加载图片
-	loadTextureFromResource("container.jpg", GL_RGB, GL_RGB);
+		// 加载图片
+		loadTextureFromResource("container.jpg", GL_RGB, GL_RGB);
 
-	glActiveTexture(GL_TEXTURE1);
-	glBindTexture(GL_TEXTURE_2D, texture2);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
 
-	// 设置纹理边缘的重复规则
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	// 设置纹理过滤
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		// 设置纹理边缘的重复规则
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		// 设置纹理过滤
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	loadTextureFromResource("awesomeface.png", GL_RGBA, GL_RGBA);
+		loadTextureFromResource("awesomeface.png", GL_RGBA, GL_RGBA);
+	}
 
 	shader.use();
 	shader.setInt("texture1", 0);
@@ -143,8 +146,8 @@ int main()
 
 	float mixValue = 0.0f;
 
-	while (!glfwWindowShouldClose(window)) {
-
+	while (!glfwWindowShouldClose(window))
+	{
 		// ---------------------
 		// 清除屏幕
 		glClearColor(0, 1, 0.5, 1);
@@ -156,7 +159,7 @@ int main()
 		if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
 		{
 			mixValue += 0.01f;
-			if (mixValue >= 1.0f) 
+			if (mixValue >= 1.0f)
 			{
 				mixValue = 1.0f;
 			}
@@ -178,10 +181,26 @@ int main()
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture2);
 
-		// 开始绘制
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		// ---------------------
+
+		// 旋转
+		{
+			glm::mat4 rot(1.0f);
+			rot = glm::rotate(rot, static_cast<float>(glfwGetTime()), glm::vec3(0.0, 0.0, 1.0));
+			shader.setMatrix4("transform", glm::value_ptr(rot));
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+		}
+
+		// 第二个
+		{
+			glm::mat4 trans(1.0f);
+			trans = glm::translate(trans, glm::vec3(-0.5f, 0.5f, 0.0f));
+			const auto scale = static_cast<float>(glm::sin(glfwGetTime()))+1;
+			trans = glm::scale(trans, glm::vec3(scale, scale, scale));
+
+			shader.setMatrix4("transform", glm::value_ptr(trans));
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+		}
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
--- Tool/Shader.h
+++ Tool/Shader.h
@@ -92,7 +92,7 @@ public:
             glDeleteShader(geometry);
 	}
 
-    void use() 
+    void use() const
     {
         glUseProgram(ID);
     }
@@ -106,6 +106,11 @@ public:
     {
         glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
     }
+
+    void setMatrix4(const std::string& name, const float* value) const
+	{
+        glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, value);
+	}
 
     ~Shader() 
     {
